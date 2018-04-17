7d7409c063a7d112b7585f4afa3e436968c5679d
daniel258 <danielnevo@gmail.com>
2018-04-17 08:30:41 -0400

    Added cpp function to find first "1" in a numeric vector

---
+++ src/FirstOne.cpp
@@ -0,0 +1,16 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+
+// [[Rcpp::export]]
+int FirstOne(NumericVector x)
+  {
+  int index = 0;
+  int length = x.size();
+  for (int i = 0; i < length; i++) {
+    if(x[i]==1) {
+      index = i + 1;
+      break;
+    }}
+  return index;
+}
