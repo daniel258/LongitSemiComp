ca0eaf683aabafa0d24d7d32ffbc2ca35b0fbe43
Daniel Nevo <nhdne@pc002083.bwh.harvard.edu>
2018-06-19 15:46:01 -0400

    Added wrap function LongitSC, and print method inital version

---
+++ R/LongitSC.R
@@ -0,0 +1,65 @@
+LongitSC <- function(longit.data, times = NULL, formula.NT, formula.T, formula.OR, data, epsOr = 10^(-10),
+                     knots = NULL, lambda = 0, init = NULL, maxit.optim = 50000)
+{
+  m <- match.call()
+  if (is.null(knots)) m$knots <- 5
+  XNTmat <- model.matrix(formula.NT, data = data)[, -1] %>% as.matrix
+  XTmat <- model.matrix(formula.T, data = data)[, -1] %>% as.matrix
+  XORmat <- model.matrix(formula.OR, data = data)[, -1] %>% as.matrix
+  if(length(lambda)==1) m$lambda <- rep(lambda, 3)
+  m$J <- ncol(longit.data$YNT)
+  if (is.null(times)) times <- 1:m$J
+  smooth.aux <- mgcv::smooth.construct.ps.smooth.spec(mgcv::s(times,bs="ps", k = m$knots), data = list(times = times),
+                                                      knots = list(times = c(min(times),max(times))))
+  S.penal <- smooth.aux$S[[1]]
+  Bsplines <- smooth.aux$X
+  Q <- ncol(Bsplines)
+  pNT <- ncol(XNTmat)
+  pT <- ncol(XTmat)
+  pOR <- ncol(XORmat)
+  p <- pNT + pT + pOR
+  n.params <- 1 + 3*Q + p
+ if (is.null(init))
+ {
+   init <- rep(0,n.params)
+ }
+  res.opt <- optim(par = init, fn = PenalLogLik, gr = GradPenalLogLik, hessian = T,
+                    control = list(maxit = maxit.optim), method = "L-BFGS-B", epsOR = epsOr,
+                    XNT = XNTmat, XT = XTmat, XOR = XORmat,
+                    YNT = longit.data$YNT, YT = longit.data$YT,
+                    riskNT = longit.data$risk.NT, riskT = longit.data$risk.T,
+                    TimeBase = Bsplines,
+                    TimePen = S.penal, lambda = lambda)
+  fit <- list()
+  fit$NT.varnames <- colnames(XNTmat)
+  fit$T.varnames <- colnames(XTmat)
+  fit$OR.varnames <- colnames(XORmat)
+  fit$Bsplines <- Bsplines
+  fit$optim.conv <- res.opt$convergence
+  fit$est <- res.opt$par
+  fit$se.naive <- sqrt(diag(solve(res.opt$hessian)))
+  fit$formula.NT <- formula.NT
+  fit$formula.T <- formula.T
+  fit$formula.OR <- formula.OR
+  my.grad.sqrd <- GradPenalLogLikPers(param = res.opt$par, epsOR = epsOr,
+                                       XNT = XNTmat, XT = XTmat, XOR = XORmat,
+                                       YNT = longit.data$YNT, YT = longit.data$YT,
+                                       riskNT = longit.data$risk.NT, riskT = longit.data$risk.T,
+                                       TimeBase = Bsplines,
+                                       TimePen = S.penal, lambda = lambda)
+  fit$v.hat <- solve(res.opt$hessian)%*%my.grad.sqrd%*%(solve(res.opt$hessian))
+  fit$se.rob <- sqrt(diag(fit$v.hat))
+  fit$coef.longterm <-  fit$est[1]
+  fit$time.int.NT <- expit(Bsplines%*%fit$est[2:(1 + Q)])
+  fit$time.int.T <- expit(Bsplines%*%fit$est[(1 + Q + 1):(1 + 2*Q)])
+  fit$time.int.OR <- exp(Bsplines%*%fit$est[(1 + 2*Q + 1):(1 + 3*Q)])
+  fit$coef.NT <- fit$est[(1 + 3*Q + 1):(1 + 3*Q + pNT)]
+  fit$coef.T <- fit$est[(1 + 3*Q + pNT + 1):(1 + 3*Q + pNT + pT)]
+  fit$coef.OR <- fit$est[(1 + 3*Q + pNT + pT + 1):(1 + 3*Q + pNT + pT + pOR)]
+  fit$se.rob.NT <- fit$se.rob[(1 + 3*Q + 1):(1 + 3*Q + pNT)]
+  fit$se.rob.T <- fit$se.rob[(1 + 3*Q + pNT + 1):(1 + 3*Q + pNT + pT)]
+  fit$se.rob.OR <- fit$se.rob[(1 + 3*Q + pNT + pT + 1):(1 + 3*Q + pNT + pT + pOR)]
+  names(fit$coef.NT) <- names(fit$se.rob.NT) <- colnames(XNTmat)
+  class(fit) <- "LongitSC"
+  fit
+}
---
+++ R/Methods.R
@@ -0,0 +1,108 @@
+substrRight <- function(x, n=5){
+  substr(x, nchar(x)-n+1, nchar(x)) ### Thank you Andrie (http://stackoverflow.com/questions/7963898/extracting-the-last-n-characters-from-a-string-in-r)
+}
+substrRightStar <- function(x, n=6){
+  substr(x, nchar(x)-n+1, nchar(x)-1) ### Thank you Andrie (http://stackoverflow.com/questions/7963898/extracting-the-last-n-characters-from-a-string-in-r)
+}
+#' @export
+print.LongitSC <- function(fit, digits = max(options()$digits - 4, 3),...)
+{
+  cat("formula.NT:\n")
+  print(fit$formula.NT)
+  cat("\n")
+  cat("formula.T:\n")
+  print(fit$formula.T)
+  cat("\n")
+  cat("formula.OR:\n")
+  print(fit$formula.OR)
+  cat("\n")
+  coef.NT <- fit$coef.NT
+    coef.NT.names <- fit$NT.varnames
+    sd.err.NT <- fit$se.rob.NT
+    zvalue.NT <- coef.NT/sd.err.NT
+    pvalue.NT <- 2*pnorm(abs(zvalue.NT),lower.tail = F)
+    coef.table <- cbind(coef.NT, sd.err.NT, zvalue.NT, pvalue.NT)
+    dimnames(coef.table) <- list(coef.NT.names,
+                                 c("Estimate", "Std. Error", "z value", "Pr(>|z|)"))
+    printCoefmat(coef.table, digits = digits)
+}
+  # } else if (x$pres=="sep") {
+  #   cat("------------------------------------------------------------------------------")
+  #   cat("\n------------------------------------------------------------------------------")
+  #   cat("\nMarginal Model (Model without the Mediator):\n")
+  #   coeffs <- x$GEEfit$coefficients
+  #   coeffs.names <- names(coeffs)
+  #   #stars <- which(sapply(coeffs.names, substrRight)==".star" | sapply(coeffs.names, substrRightStar)==".star")
+  #   #no.stars <- which(sapply(coeffs.names, substrRight)!=".star" & sapply(coeffs.names, substrRightStar)!=".star")
+  #   stars <- which(sapply(coeffs.names, function(x) grepl(".star", x)))
+  #   no.stars <- which(!sapply(coeffs.names, function(x) grepl(".star", x)))
+  #   covmat <- x$GEEfit$robust.variance
+  #   sd.err <- sqrt(diag(covmat))
+  #   zvalue <- coeffs/sd.err
+  #   pvalue <- 2*pnorm(abs(zvalue),lower.tail = F)
+  #   coef.table <- cbind(coeffs, sd.err, zvalue, pvalue)
+  #   dimnames(coef.table) <- list(names(coeffs),
+  #                                c("Estimate", "Std. Error", "z value", "Pr(>|z|)"))
+  #   cond.table <- coef.table[no.stars,]
+  #   rownames(cond.table)[1] <- "(Intercept)"
+  #   marg.table <- coef.table[stars,]
+  #   rownames(marg.table)[1] <- "(Intercept)"
+  #   rownames(marg.table)[-1] <- sapply(rownames(marg.table)[-1], function(x) gsub(pattern = ".star", "", x))
+  #   #cat(row.names(marg.table), "\n")
+  #   stats::printCoefmat(marg.table, digits = digits)
+  #   cat("\n------------------------------------------------------------------------------")
+  #   cat("\n------------------------------------------------------------------------------")
+  #   cat("\nConditional Model (Model with the Mediator):\n \n")
+  #   stats::printCoefmat(cond.table,  digits = digits)
+  # }
+  # cat("\n---------------------------")
+  # cat("\nNatural Indirect Effect: ", format(x$nie, digits = digits),
+  #     "\np=", format(x$nie.pval, digits = 2),
+  #     " for ", paste0(x$alter), " test for mediation \n", sep = "")
+  # cat("Confidence Interval = [", format(x$nie.ci[1],digits = digits),",",format(x$nie.ci[2],digits = digits),"]", sep = "")
+  # cat("\n---------------------------")
+  # cat("\nNatural Direct Effect:", format(x$nde, digits = digits))
+  # cat("\n---------------------------")
+  # if(x$pm >=0 & x$pm < 1)
+  # {
+  # cat("\nMediation Proportion:", format(100*x$pm,digits = 3),"%",
+  #     "\np=", format.pval(x$pm.pval, digits = 2),
+  #     " for one-sided test for mediation \n", sep = "")
+  #
+  # cat("Confidence Interval = [", format(100*x$pm.ci[1],digits = 3),"%",",",format(100*x$pm.ci[2],digits = 3),"%","]", sep = "")
+  # } else
+  # {
+  #   cat("\nMediation Proportion:", format(100*x$pm,digits = 3),"%")
+  # }
+  #  cat("\n---------------------------")
+#}
+# predict.GEEmediate <- function (object, newdata = NULL, model.pred = c("cond", "marg"),type = c("link", "response", "terms"),
+#                                 se.fit = FALSE, dispersion = NULL, terms = NULL, na.action = na.pass, ...)
+# {
+#   model.pred <- match.arg(model.pred)
+#   type <- match.arg(type)
+#   if (se.fit==T) {warning("se.fit=T is currently not supported for GEEmediate")}
+#   if (type=="terms") {
+#     warning("type='terms' is currently not supported for GEEmediate, using type='response' intead")
+#     type <- "response"
+#   }
+#   gee.object <- object$GEEfit
+#   if(!missing(newdata))
+#   {
+#     newdf <- newdata[gee.object$xnames[substrRight(gee.object$xnames)!=".star" & substrRight(gee.object$xnames)!="INT"]]
+#     dupl.df.new <- DupliData(df = newdf, mediator = object$call[[4]], surv = F)
+#     dupl.df.new <- dupl.df.new[gee.object$xnames]
+#     eta <- gee.object$linear.predictors
+#     if (type=="response") {
+#     pred <- make.link(tolower(gee.object$model$link))$linkinv(eta)
+#     } else {
+#       pred <- eta
+#     }
+#     if (model.pred=="cond") {out <- pred[1:(gee.object$nobs/2)]}
+#     if (model.pred=="marg") {out <- pred[(gee.object$nobs/2+1):gee.object$nobs]}
+#   } else {
+#     pred <- predict.glm(object = gee.object, type = type, se.fit = F, na.action = na.action, ...)
+#     if (model.pred=="cond") {out <- pred[1:(gee.object$nobs/2)]}
+#     if (model.pred=="marg") {out <- pred[(gee.object$nobs/2+1):gee.object$nobs]}}
+#   out
+# }
