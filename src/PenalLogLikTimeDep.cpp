904108bd444a501cbe4244519867cf5a419edc79
daniel258 <your_email@example.com>
2019-08-19 16:50:04 +0300

    Update, new PenalLogLikTimeDep function

--- src/PenalLogLikTimeDep.cpp
+++ src/PenalLogLikTimeDep.cpp
@@ -4,39 +4,40 @@ using namespace Rcpp;
 // [[Rcpp::depends(RcppArmadillo)]]
 //' @export
 // [[Rcpp::export]]
-double PenalLogLikTimeDepOLD(arma::vec param, arma::mat YT, arma::mat YNT, arma::mat riskNT, arma::mat riskT, 
+double PenalLogLikTimeDep(arma::vec param, arma::vec ID, arma::uvec TM,
+                      arma::vec YT, arma::vec YNT, 
                    arma::mat XNT, arma::mat XT, arma::mat XOR,
-                   arma::mat XNTtimeDep, arma::mat XTtimeDep, arma::mat XORtimeDep,
                    arma::mat TimeBase, arma::mat TimePen, arma::vec lambda, double epsOR)
 {
+  //arma::mat XNTtimeDep, arma::mat XTtimeDep, arma::mat XORtimeDep,
   //int pNTtimeDep, int pTtimeDep, int pORtimeDep
-  int n = YT.n_rows;
-  int J = YT.n_cols;
+  //int n = max(ID); // ID must be
+  arma::vec IDunq = arma::unique(ID); // IDs once
+  int n = IDunq.size(); // sample size
+  arma::uvec TMunq = arma::unique(TM); // unique "times" (intervals)
+ // int J = TMunq.size(); // number of "times" (intervals)
   int pNT = XNT.n_cols;
   int pT = XT.n_cols;
   int pOR = XOR.n_cols;
   int Q = TimeBase.n_cols; // Q is the the number of B-splines (number of rows in TimeBase should be J)
-  // Decomposing "param" to individual paramters 
+  // Decomposing "param" to individual paramters
   // Current verison of the code: only one time-dep variable (but throgout the code, I made preperations for generalizing it)
-  int pNTtimeDep = 1;
-  int pTtimeDep = 1;
-  int pORtimeDep = 1;
-  double gamma = param[0];
+  //int pNTtimeDep = 1;
+  //int pTtimeDep = 1;
+  //int pORtimeDep = 1;
+  double betay = param[0];
   arma::vec alphaNT = param.subvec(1,Q);
   arma::vec alphaT = param.subvec(Q+1,2*Q);
   arma::vec alphaOR = param.subvec(2*Q+1,3*Q);
   arma::mat penaltermNT = lambda[0] * alphaNT.t() * TimePen * alphaNT;
   arma::mat penaltermT = lambda[1] * alphaT.t() * TimePen * alphaT;
   arma::mat penaltermOR = lambda[2] * alphaOR.t() * TimePen * alphaOR;
-  // beta order is betaNT,betaNTtimeDep, betaT, betaTtimeDep, betaOR, betaORtimeDep
   arma::vec betaNT = param.subvec(3*Q + 1, 3*Q + pNT);
-  arma::vec betaNTtimeDep = param.subvec(3*Q + pNT +  1, 3*Q + pNT + pNTtimeDep);
-  arma::vec betaT = param.subvec(3*Q + pNT + pNTtimeDep + 1, 3*Q + pNT + pNTtimeDep + pT);
-  arma::vec betaTtimeDep = param.subvec(3*Q + pNT + pNTtimeDep + pT + 1, 3*Q + pNT + pNTtimeDep + pT + pTtimeDep);
-  arma::vec betaOR = param.subvec(3*Q + pNT + pNTtimeDep + pT + pTtimeDep + 1, 3*Q + pNT + pNTtimeDep + pT + pTtimeDep + pOR);
-  arma::vec betaORtimeDep = param.subvec(3*Q + pNT + pNTtimeDep + pT + pTtimeDep + pOR + 1, 
-                                         3*Q + pNT + pNTtimeDep + pT + pTtimeDep + pOR + pORtimeDep);
-//  Rcpp::Rcout << "betaTtimeDep:"<< betaTtimeDep << std::endl;
+  arma::vec betaT = param.subvec(3*Q + pNT + 1, 3*Q + pNT + pT);
+  arma::vec betaOR = param.subvec(3*Q + pNT + pT + 1, 3*Q + pNT + pT + pOR);
+  // Rcpp::Rcout << "betaNT:  "<< betaNT << std::endl;
+  // Rcpp::Rcout << "betaT:  "<< betaT << std::endl;
+  // Rcpp::Rcout << "betaOR:  "<< betaOR << std::endl;
   double loglik = 0;
   double iContrib = 0;
   double iProbTafterNT = 0;
@@ -47,62 +48,86 @@ double PenalLogLikTimeDepOLD(arma::vec param, arma::mat YT, arma::mat YNT, arma:
   double ExpAlphaNTnow = 0;
   double ExpAlphaTnow = 0;
   double ExpAlphaORnow = 0;
-  arma::vec ExpXBetaNTfixed = exp(XNT * betaNT);
-  arma::vec ExpXBetaTfixed = exp(XT * betaT);
-  arma::vec ExpXBetaORfixed = exp(XOR * betaOR);
+  double ExpXBetaNTnow = 0;
+  double ExpXBetaTnow = 0;
+  double ExpXBetaORnow = 0;
   arma::vec ExpAlphaNT = exp(TimeBase * alphaNT);
   arma::vec ExpAlphaT = exp(TimeBase * alphaT);
   arma::vec ExpAlphaOR = exp(TimeBase * alphaOR);
-  for (int j = 0; j < J; ++j)
+  arma::vec ExpXBetaNT = exp(XNT * betaNT);
+  arma::vec ExpXBetaT = exp(XT * betaT);
+  arma::vec ExpXBetaOR = exp(XOR * betaOR);
+  //Rcpp::Rcout << "ExpXBetaOR[0]:  "<< ExpXBetaOR[0] << std::endl;
+  for (int i = 0; i < n; ++i)
   {
-    ExpAlphaNTnow = ExpAlphaNT[j];
-    ExpAlphaTnow = ExpAlphaT[j];
-    ExpAlphaORnow = ExpAlphaOR[j];
-    // Take current value of time-dep variables
-    arma::vec ExpXBetaNTtimeDep = arma::zeros(n);
-    arma::vec ExpXBetaTtimeDep = arma::zeros(n);
-    arma::vec ExpXBetaORtimeDep = arma::zeros(n);
-    arma::vec XNTtimeDepNow = XNTtimeDep.col(j);
-    arma::vec XTtimeDepNow = XTtimeDep.col(j);
-    arma::vec XORtimeDepNow = XORtimeDep.col(j);
-    for(int k = 0; k < n; ++k)
-    {
-      arma::uvec indexNT =  as<arma::uvec>(wrap(Rcpp::Range((k-1)*pNTtimeDep + 1, k*pNTtimeDep)));
-      arma::uvec indexT =  as<arma::uvec>(wrap(Rcpp::Range((k-1)*pTtimeDep + 1, k*pTtimeDep)));
-      arma::uvec indexOR =  as<arma::uvec>(wrap(Rcpp::Range((k-1)*pORtimeDep + 1, k*pORtimeDep)));
-      arma::rowvec tempNT = XNTtimeDepNow(indexNT);
-      arma::rowvec tempT = XTtimeDepNow(indexT);
-      arma::rowvec tempOR = XORtimeDepNow(indexOR);
-     // Rcpp::Rcout << "tempT:"<< tempT << std::endl;
-      ExpXBetaNTtimeDep[k] = exp(as_scalar(tempNT * betaNTtimeDep));
-      ExpXBetaTtimeDep[k] = exp(as_scalar(tempT * betaTtimeDep));
-      ExpXBetaORtimeDep[k] = exp(as_scalar(tempOR * betaORtimeDep));
-    }
-    for (int i = 0; i < n; ++i)
-    {
-      if (riskT(i,j)==0) {
-        iContrib=0;
-        //   nocontrib
-      } else {
-        if (riskNT(i,j)==0) {
-          iProbTafterNT = (ExpAlphaTnow*ExpXBetaTfixed[i]*ExpXBetaTtimeDep[i]*exp(gamma))/
-            (1 + (ExpAlphaTnow*ExpXBetaTfixed[i]*ExpXBetaTtimeDep[i]*exp(gamma)));
-        //  Rcpp::Rcout << "i:"<< i + 1 << std::endl;
-        //  Rcpp::Rcout << "ExpXBetaTtimeDep:"<< ExpXBetaTtimeDep[i] << std::endl;
-            if(YT(i,j)==1) {
+//    Rcpp::Rcout << "i =   "<< i << std::endl;
+    int iID = IDunq[i]; // get observation ID
+    // Get all data on that observation
+    arma::uvec pos = find(ID==iID); // rows associated with the observation
+    arma::vec iYNT = YNT.elem(pos); // YNT for that observation
+    arma::vec iYT = YT.elem(pos); // YT for that observation
+    arma::uvec iTM = TM.elem(pos); // TM for that observation, they are already unique
+    arma::vec iExpXBetaNT = ExpXBetaNT.elem(pos); // exp(X%*%\beta) for NT, for that observation (all times)
+    arma::vec iExpXBetaT = ExpXBetaT.elem(pos); // exp(X%*%\beta) for T, for that observation (all times)
+    arma::vec iExpXBetaOR = ExpXBetaOR.elem(pos); // exp(X%*%\beta) for OR, for that observation (all times)
+    // if (i==0) {
+    //   Rcpp::Rcout << "iID:  "<< iID << std::endl;
+    //   Rcpp::Rcout << "iExpXBetaNT:  "<< iExpXBetaNT << std::endl;
+    //   Rcpp::Rcout << "iExpXBetaT:  "<< iExpXBetaT << std::endl;
+    //   Rcpp::Rcout << "iExpXBetaOR:  "<< iExpXBetaOR << std::endl;
+    //   //  Rcpp::Rcout << "iTM:  "<< TM << std::endl;
+    //   
+    // }
+   // Rcpp::Rcout << "iExpXBetaOR =   "<< iExpXBetaOR << std::endl;
+    // arma::mat iXNT = XNT.rows(pos);
+    // arma::mat iXT = XT.rows(pos);
+    // arma::mat iXOR = XOR.rows(pos);
+  //  Rcpp::Rcout << "iTM =   "<< iTM << std::endl;
+  //  Rcpp::Rcout << "TMunq =   "<< TMunq << std::endl;
+ //   arma::uvec iposTM = find(TMunq==iTM);
+    //Rcpp::Rcout << "iposTM =   "<< iposTM << std::endl;
+    arma::vec iExpAlphaNT = ExpAlphaNT.elem(iTM - 1);
+    arma::vec iExpAlphaT = ExpAlphaT.elem(iTM - 1);
+    arma::vec iExpAlphaOR = ExpAlphaOR.elem(iTM - 1);
+    // if (i==0) {
+    //   Rcpp::Rcout << "iID:  "<< iID << std::endl;
+    //   Rcpp::Rcout << "iExpAlphaNT:  "<< iExpAlphaNT << std::endl;
+    //   Rcpp::Rcout << "iExpAlphaT:  "<< iExpAlphaT << std::endl;
+    //   Rcpp::Rcout << "iExpAlphaOR:  "<< iExpAlphaOR << std::endl;
+    //   //  Rcpp::Rcout << "iTM:  "<< TM << std::endl;
+    //   
+    // }
+    int iJ = iTM.size(); // number of "times" (intervals)
+   // Rcpp::Rcout << "iJ =   "<< iJ << std::endl;
+    arma::vec iRiskNT = arma::ones<arma::vec>(iJ);
+   // iRiskNT[0]=1; // no prevlant cases
+    // Rcpp::Rcout << "iYT:  "<< iYT << std::endl;
+  for (int j = 0; j < iJ; ++j)
+  {
+  //  Rcpp::Rcout << "j =   "<< j << std::endl;
+    int jTM = iTM[j]-1;
+ //   Rcpp::Rcout << "jTM =   "<< jTM << std::endl;
+    ExpAlphaNTnow = iExpAlphaNT[jTM];
+    ExpAlphaTnow = iExpAlphaT[jTM];
+    ExpAlphaORnow = iExpAlphaOR[jTM];
+    ExpXBetaNTnow = iExpXBetaNT[j];
+    ExpXBetaTnow = iExpXBetaT[j];
+    ExpXBetaORnow = iExpXBetaOR[j];
+    if (iRiskNT[j]==0) {
+          iProbTafterNT = (ExpAlphaTnow*ExpXBetaTnow*exp(betay)) / (1 + (ExpAlphaTnow*ExpXBetaTnow*exp(betay)));
+            if(iYT[j]==1) {
             iContrib = log(iProbTafterNT);
+            // Rcpp::Rcout << "iYT[j] =  "<< iYT[j] << std::endl;
           }
           else {
             iContrib = log(1-iProbTafterNT);
+            // Rcpp::Rcout << "iProbTafterNT =  "<< iProbTafterNT << std::endl;
+            // Rcpp::Rcout << "iYT[j] =  "<< iYT[j] << std::endl;
           }
-      //    Rcpp::Rcout << "iProbTafterNT  "<< iProbTafterNT << std::endl;
         } else {
-          iProb1 = ExpAlphaNTnow*ExpXBetaNTfixed[i]*ExpXBetaNTtimeDep[i]/(1 + (ExpAlphaNTnow*ExpXBetaNTfixed[i]*ExpXBetaNTtimeDep[i]));
-          iProb2 = ExpAlphaTnow*ExpXBetaTfixed[i]*ExpXBetaTtimeDep[i]/(1 + ExpAlphaTnow*ExpXBetaTfixed[i]*ExpXBetaTtimeDep[i]);
-          iOR = ExpAlphaORnow*ExpXBetaORfixed[i]*ExpXBetaORtimeDep[i];
-           // Rcpp::Rcout << "iProb1  "<< iProb1 << std::endl;
-           // Rcpp::Rcout << "iProb2 "<< iProb2 << std::endl;
-           // Rcpp::Rcout << "iOR "<< iOR << std::endl;
+          iProb1 = ExpAlphaNTnow*ExpXBetaNTnow/(1 + (ExpAlphaNTnow*ExpXBetaNTnow));
+          iProb2 = ExpAlphaTnow*ExpXBetaTnow/(1 + ExpAlphaTnow*ExpXBetaTnow);
+          iOR = ExpAlphaORnow*ExpXBetaORnow;
           if ((iOR > 1 - epsOR) & (iOR < 1 + epsOR))
           {
             iProb12 = iProb1*iProb2;
@@ -110,23 +135,38 @@ double PenalLogLikTimeDepOLD(arma::vec param, arma::mat YT, arma::mat YNT, arma:
             iProb12 = (1 + (iProb1 + iProb2)*(iOR - 1) - sqrt(pow(1 + (iProb1 + iProb2)*(iOR - 1), 2.0) -
               4*iOR*(iOR - 1)*iProb1*iProb2)) / (2 * (iOR - 1));
           }
-          
           // Rcpp::Rcout << "iProb12  "<< iProb12 << std::endl;
-          if (YNT(i,j)==1 && YT(i,j)==1) {
+          if (iYNT[j]==1 && iYT[j]==1) {
             iContrib = log(iProb12);
           }
-          if (YNT(i,j)==1 && YT(i,j)==0) {
+          if (iYNT[j]==1 && iYT[j]==0) {
             iContrib = log(iProb1 - iProb12);
+            if (j < iJ) { 
+             iRiskNT[j+1] = 0;}
           }
-          if (YNT(i,j)==0 && YT(i,j)==1) {
+          if (iYNT[j]==0 && iYT[j]==1) {
             iContrib = log(iProb2 - iProb12);
           }
-          if (YNT(i,j)==0 && YT(i,j)==0) {
+          if (iYNT[j]==0 && iYT[j]==0) {
             iContrib = log(1 - iProb1 - iProb2 + iProb12);
           }
-        }}
-      loglik += iContrib;
-    }}
+        }
+          // if (i==0)
+          // {
+          // Rcpp::Rcout << "i =   "<< i << std::endl;
+          // Rcpp::Rcout << "j =   "<< j << std::endl;
+          // Rcpp::Rcout << "iRiskNT[j] =  "<< iRiskNT[j] << std::endl;
+          // Rcpp::Rcout << "iYNT[j] =  "<< iYNT[j] << std::endl;
+          // Rcpp::Rcout << "iYT[j] =  "<< iYT[j] << std::endl;
+          // Rcpp::Rcout << "iProbTafterNT =  "<< iProbTafterNT << std::endl;
+          // Rcpp::Rcout << "iProb1 =  "<< iProb1 << std::endl;
+          // Rcpp::Rcout << "iProb2 =  "<< iProb2 << std::endl;
+          // Rcpp::Rcout << "iProb12 =  "<< iProb12 << std::endl;
+          // Rcpp::Rcout << "iContrib =  "<< iContrib << std::endl;
+          // }
+        loglik += iContrib;
+  }}
     double  penalloglik = loglik - as_scalar(penaltermNT) - as_scalar(penaltermT) - as_scalar(penaltermOR);
+  // Rcpp::Rcout << "penalloglik =  "<< penalloglik << std::endl;
     return(-penalloglik);
 }
