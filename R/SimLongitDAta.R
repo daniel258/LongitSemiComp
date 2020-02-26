ca792fe82e678442110abec24fcda0b0887755c7
daniel258 <your_email@example.com>
2020-02-26 15:16:05 +0200

    Align code with notation: J => K ; j => k

--- R/SimLongitDAta.R
+++ R/SimLongitDAta.R
@@ -32,7 +32,7 @@
 #' beta.t <- log(c(0.5, 1))
 #' beta.or <- log(c(1, 1))
 #' beta.y <- log(1.4)
-#'   my.data <- SimLongitDataSmooth(n.sample = 200, times = times,  beta.y,  alpha.nt, alpha.t, alpha.or, beta.nt, beta.t, beta.or)
+#'   my.data <- SimLongitData(n.sample = 200, times = times,  beta.y,  alpha.nt, alpha.t, alpha.or, beta.nt, beta.t, beta.or)
 #'    longit.data <- my.data[-1]
 #'    X <- my.data[1]
 # \dontrun{
@@ -47,12 +47,12 @@ SimLongitData <- function(n.sample, times = 1:100,  beta.y,  alpha.nt, alpha.t, 
   if (length(alpha.nt) != length(times)) {stop("alpha.nt should be in the same length as times")}
   if (length(alpha.t) != length(times)) {stop("alpha.t should be in the same length as times")}
   if (length(alpha.or) != length(times)) {stop("alpha.or should be in the same length as times")}
-  J <- length(times)
+  K <- length(times)
   p <- length(beta.nt)
   if (is.null(X)) {
   X <- matrix(nrow = n.sample, ncol = p, rnorm(n.sample*p))}
   if (!all(dim(X)==c(n.sample,p))) {stop("X should have n.sample rows and number of columns identical to length(beta.nt)")} 
-  risk.NT <- risk.T <- YNT <- YT <- matrix(nrow = n.sample, ncol = J, 0)
+  risk.NT <- risk.T <- YNT <- YT <- matrix(nrow = n.sample, ncol = K, 0)
   risk.NT[,1] <- 1
   risk.T[,1] <- 1
   p.nt.first <- expit(alpha.nt[1] + X%*%beta.nt)
@@ -64,33 +64,33 @@ SimLongitData <- function(n.sample, times = 1:100,  beta.y,  alpha.nt, alpha.t, 
   YT[,1] <- crude.data %in% c(3,4)
   YNT[YNT[,1]==1, 2] <- 1
   YT[YT[,1]==1, 2] <- 1
-  for(j in 2:J)
+  for(k in 2:K)
   {
-    risk.NT[,j] <- (YNT[,j-1]==0 & YT[,j-1]==0)
-    risk.T[,j] <- 1*(YT[,j-1]==0)
-    at.risk.T.only <- risk.NT[,j]==0 & risk.T[,j]==1
-    at.risk.both <- risk.NT[,j]==1 & risk.T[,j]==1
+    risk.NT[,k] <- (YNT[, k-1]==0 & YT[, k-1]==0)
+    risk.T[,k] <- 1*(YT[, k-1]==0)
+    at.risk.T.only <- risk.NT[, k]==0 & risk.T[, k]==1
+    at.risk.both <- risk.NT[, k]==1 & risk.T[, k]==1
     # at risk for terminal event only
     if (sum(at.risk.T.only)>0)
     {
-      probs.T.only <- expit(alpha.t[j] + X[at.risk.T.only, ]%*%beta.t + beta.y)
-      YT[at.risk.T.only, j] <- rbinom(sum(at.risk.T.only), 1, probs.T.only)
+      probs.T.only <- expit(alpha.t[k] + X[at.risk.T.only, ]%*%beta.t + beta.y)
+      YT[at.risk.T.only, k] <- rbinom(sum(at.risk.T.only), 1, probs.T.only)
     }
     #at risk for both events
-    if  (sum(at.risk.both)>0)
+    if  (sum(at.risk.both) > 0)
     {
-      p.nt.both <- expit(alpha.nt[j] + X[at.risk.both, ]%*%beta.nt)
-      p.t.both <- expit(alpha.t[j] + X[at.risk.both, ]%*%beta.t)
-      OR.both <- exp(alpha.or[j]+ X[at.risk.both, ]%*%beta.or)
+      p.nt.both <- expit(alpha.nt[k] + X[at.risk.both, ]%*%beta.nt)
+      p.t.both <- expit(alpha.t[k] + X[at.risk.both, ]%*%beta.t)
+      OR.both <- exp(alpha.or[k]+ X[at.risk.both, ]%*%beta.or)
       probs.both <- MargORtoJoint(p1marg = p.nt.both, p2marg = p.t.both, OR = OR.both)
       crude.data.both <- apply(X = probs.both, MARGIN = 1, FUN = sample, x = 1:4, size = 1, replace = T)
-      YNT[at.risk.both, j] <- crude.data.both %in% c(2, 4)
-      YT[at.risk.both, j] <- crude.data.both %in% c(3, 4)
+      YNT[at.risk.both, k] <- crude.data.both %in% c(2, 4)
+      YT[at.risk.both, k] <- crude.data.both %in% c(3, 4)
     }
-    if(j < J)  {
-      YNT[YNT[, j]==1, j + 1] <- 1
-      YT[YT[, j]==1, j + 1] <- 1
+    if(k < K)  {
+      YNT[YNT[, k]==1, k + 1] <- 1
+      YT[YT[, k]==1, k + 1] <- 1
     }
   }
-  return(list(X =X, YNT = YNT, YT = YT, risk.NT = risk.NT, risk.T = risk.T))
+  return(list(X = X, YNT = YNT, YT = YT, risk.NT = risk.NT, risk.T = risk.T))
 }
--- R/SimLongitDataTimeDep.R
+++ R/SimLongitDataTimeDep.R
@@ -32,7 +32,7 @@
 #' beta.t <- log(c(0.5, 1))
 #' beta.or <- log(c(1, 1))
 #' beta.y <- log(1.4)
-#'  my.data <- SimLongitDataTimeDep(n.sample, times = times,  beta.y,  alpha.nt, alpha.t, alpha.or, 
+#'  my.data <- SimLongitDataTimeDep(n.sample = 2000, times = times,  beta.y,  alpha.nt, alpha.t, alpha.or, 
 #'                                  beta.nt, beta.t, beta.or, cens.poten.rate = 0.5)
 #'  df.data <- my.data$df.return
 #'  mean(my.data$cens)
@@ -44,12 +44,12 @@ SimLongitDataTimeDep <- function(n.sample, times = 1:100,  beta.y,  alpha.nt, al
   if (length(alpha.nt) != length(times)) {stop("alpha.nt should be in the same length as times")}
   if (length(alpha.t) != length(times)) {stop("alpha.t should be in the same length as times")}
   if (length(alpha.or) != length(times)) {stop("alpha.or should be in the same length as times")}
-  J <- length(times)
+  K <- length(times)
   p <- length(beta.nt)
   X.time.fixed <- as.matrix(rnorm(n.sample))
-  X.time.dep <- matrix(nrow = n.sample, ncol = J, 0) 
+  X.time.dep <- matrix(nrow = n.sample, ncol = K, 0) 
   X.time.dep[, 1] <- rbinom(n.sample, 1, 0.6)  # At baseline Pr(X(t)=1)=0.6
-  risk.NT <- risk.T <- YNT <- YT <- matrix(nrow = n.sample, ncol = J, 0)
+  risk.NT <- risk.T <- YNT <- YT <- matrix(nrow = n.sample, ncol = K, 0)
   risk.NT[,1] <- 1
   risk.T[,1] <- 1
   Xfirst <- cbind(X.time.fixed, X.time.dep[,1])
@@ -62,47 +62,47 @@ SimLongitDataTimeDep <- function(n.sample, times = 1:100,  beta.y,  alpha.nt, al
   YT[,1] <- crude.data %in% c(3,4)
   YNT[YNT[,1]==1, 2] <- 1
   YT[YT[,1]==1, 2] <- 1
-  for(j in 2:J)
+  for(k in 2:K)
   {
-    Xnow <- X.time.dep[ ,j - 1]
+    Xnow <- X.time.dep[ ,k - 1]
     Xnow[Xnow==1] <- rbinom(sum(Xnow), 1, 0.9) 
-    X.time.dep[ ,j] <- Xnow
+    X.time.dep[ ,k] <- Xnow
     Xtemp <- cbind(X.time.fixed, Xnow)
-    risk.NT[,j] <- (YNT[, j - 1]==0 & YT[, j - 1]==0)
-    risk.T[,j] <- 1*(YT[, j - 1]==0)
-    at.risk.T.only <- risk.NT[, j]==0 & risk.T[, j]==1
-    at.risk.both <- risk.NT[, j]==1 & risk.T[, j]==1
+    risk.NT[,k] <- (YNT[, k - 1]==0 & YT[, k - 1]==0)
+    risk.T[,k] <- 1*(YT[, k - 1]==0)
+    at.risk.T.only <- risk.NT[, k]==0 & risk.T[, k]==1
+    at.risk.both <- risk.NT[, k]==1 & risk.T[, k]==1
     # at risk for terminal event only
     if (sum(at.risk.T.only)>0)
     {
-      probs.T.only <- expit(alpha.t[j] + Xtemp[at.risk.T.only, ]%*%beta.t + beta.y)
-      YT[at.risk.T.only, j] <- rbinom(sum(at.risk.T.only), 1, probs.T.only) 
+      probs.T.only <- expit(alpha.t[k] + Xtemp[at.risk.T.only, ]%*%beta.t + beta.y)
+      YT[at.risk.T.only, k] <- rbinom(sum(at.risk.T.only), 1, probs.T.only) 
     }
     #at risk for both events
-    if  (sum(at.risk.both)>0)
+    if  (sum(at.risk.both) > 0)
     {
-      p.nt.both <- expit(alpha.nt[j] + Xtemp[at.risk.both, ]%*%beta.nt)
-      p.t.both <- expit(alpha.t[j] + Xtemp[at.risk.both, ]%*%beta.t)
-      OR.both <- exp(alpha.or[j]+ Xtemp[at.risk.both, ]%*%beta.or)
+      p.nt.both <- expit(alpha.nt[k] + Xtemp[at.risk.both, ]%*%beta.nt)
+      p.t.both <- expit(alpha.t[k] + Xtemp[at.risk.both, ]%*%beta.t)
+      OR.both <- exp(alpha.or[k]+ Xtemp[at.risk.both, ]%*%beta.or)
       probs.both <- MargORtoJoint(p1marg = p.nt.both, p2marg = p.t.both, OR = OR.both)
       crude.data.both <- apply(X = probs.both, MARGIN = 1, FUN = sample, x = 1:4, size = 1, replace = T)
-      YNT[at.risk.both, j] <- crude.data.both %in% c(2, 4)
-      YT[at.risk.both, j] <- crude.data.both %in% c(3, 4)
+      YNT[at.risk.both, k] <- crude.data.both %in% c(2, 4)
+      YT[at.risk.both, k] <- crude.data.both %in% c(3, 4)
     }
-    if(j < J)  {
-      YNT[YNT[, j]==1, j + 1] <- 1
-      YT[YT[, j]==1, j + 1] <- 1
+    if(k < K)  {
+      YNT[YNT[, k]==1, k + 1] <- 1
+      YT[YT[, k]==1, k + 1] <- 1
     }
   }
   # Add censoring
-  C <- sample(x = 2:J, replace = T, size = n.sample)
+  C <- sample(x = 2:K, replace = T, size = n.sample)
   somecens <- rbinom(n.sample, 1, cens.poten.rate)
   cens <- rep(0, n.sample)
   for (i in 1:n.sample)
   {
   if (somecens[i]==1) {
-  YNT[i, C[i]:J] <- YT[i, C[i]:J] <- NA
-  risk.NT[i, C[i]:J] <- risk.T[i, C[i]:J] <- 0
+  YNT[i, C[i]:K] <- YT[i, C[i]:K] <- NA
+  risk.NT[i, C[i]:K] <- risk.T[i, C[i]:K] <- 0
   if (risk.T[i, C[i] - 1]==0) {cens[i] = 1} # cens=1 if observation i was actually censored
   }}
   obs.per.person <- apply(risk.T,1, function(x) sum(x==1, na.rm = T))
