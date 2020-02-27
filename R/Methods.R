899c4c182766ef7117b4ab64802c27f3ff2e815a
daniel258 <your_email@example.com>
2020-02-27 08:49:06 +0200

    Edited methods to report Exp(estiamte) and the long-term dependnece parameters

--- R/Methods.R
+++ R/Methods.R
@@ -11,35 +11,48 @@
    print(x$formula.OR)
    cat("\n")
    cat("\n------------------------------------------------------------------------------")
-   cat("\n Non-terminal event :\n")
+   cat("\n Non-terminal event:\n")
    coef.NT <- x$coef.NT
+   exp.coef.NT <- exp(coef.NT)
    sd.err.NT <- x$se.rob.NT
    zvalue.NT <- coef.NT/sd.err.NT
    pvalue.NT <- 2*pnorm(abs(zvalue.NT),lower.tail = F)
-   coef.table.NT <- cbind(coef.NT, sd.err.NT, zvalue.NT, pvalue.NT)
-   dimnames(coef.table.NT) <- list(names(coef.NT), c("Estimate", "Std. Error", "z value", "Pr(>|z|)"))
+   coef.table.NT <- cbind(coef.NT, sd.err.NT, exp.coef.NT, zvalue.NT, pvalue.NT)
+   dimnames(coef.table.NT) <- list(names(coef.NT), c("Estimate", "Std. Error", "Exp(Estimate)", "z value", "Pr(>|z|)"))
    printCoefmat(coef.table.NT, digits = digits)
    cat("\n")
    cat("\n------------------------------------------------------------------------------")
    cat("\n Terminal event :\n")
    coef.T <- x$coef.T
-  coef.T.names <- x$T.varnames
+   exp.coef.T <- exp(coef.T)
+   coef.T.names <- x$T.varnames
    sd.err.T <- x$se.rob.T
    zvalue.T <- coef.T/sd.err.T
    pvalue.T <- 2*pnorm(abs(zvalue.T),lower.tail = F)
-   coef.table.T <- cbind(coef.T, sd.err.T, zvalue.T, pvalue.T)
-   dimnames(coef.table.T) <- list(names(coef.T), c("Estimate", "Std. Error", "z value", "Pr(>|z|)"))
+   coef.table.T <- cbind(coef.T, sd.err.T, exp.coef.T,  zvalue.T, pvalue.T)
+   dimnames(coef.table.T) <- list(names(coef.T), c("Estimate", "Std. Error", "Exp(Estimate)", "z value", "Pr(>|z|)"))
    printCoefmat(coef.table.T, digits = digits)
-    cat("\n------------------------------------------------------------------------------")
-    cat("\n Odds ratio between non-terminal and terminal events :\n")
+   cat("\n------------------------------------------------------------------------------")
+   cat("\n Odds ratio between non-terminal and terminal events :\n")
    coef.OR <- x$coef.OR
+   exp.coef.OR <- exp(coef.OR)
    coef.OR.names <- x$OR.varnames
    sd.err.OR <- x$se.rob.OR
    zvalue.OR <- coef.OR/sd.err.OR
    pvalue.OR <- 2*pnorm(abs(zvalue.OR),lower.tail = F)
-   coef.table.OR <- cbind(coef.OR, sd.err.OR, zvalue.OR, pvalue.OR)
-   dimnames(coef.table.OR) <- list(names(coef.OR), c("Estimate", "Std. Error", "z value", "Pr(>|z|)"))
+   coef.table.OR <- cbind(coef.OR, sd.err.OR, exp.coef.OR, zvalue.OR, pvalue.OR)
+   dimnames(coef.table.OR) <- list(names(coef.OR), c("Estimate", "Std. Error", "Exp(Estimate)", "z value", "Pr(>|z|)"))
    printCoefmat(coef.table.OR, digits = digits)
+   cat("\n------------------------------------------------------------------------------")
+   cat("\n Long term dependence parameter between non-terminal and terminal events :\n")
+   coef.longterm <- x$coef.longterm
+   exp.coef.longterm <- exp(coef.longterm)
+   sd.err.longterm <- x$se.longterm
+   zvalue.longterm <- coef.longterm/sd.err.longterm
+   pvalue.longterm <- 2*pnorm(abs(zvalue.longterm),lower.tail = F)
+   coef.table.longterm <- cbind(coef.longterm, sd.err.longterm, exp.coef.longterm, zvalue.longterm, pvalue.longterm)
+   dimnames(coef.table.longterm) <- list("Long-term dependence", c("Estimate", "Std. Error", "Exp(Estimate)", "z value", "Pr(>|z|)"))
+   printCoefmat(coef.table.longterm, digits = digits)
  }
  #' @export
  coef.LongitSC <- function(object, ...)
@@ -47,5 +60,6 @@
    coef.NT <- object$coef.NT
    coef.T <- object$coef.T
    coef.OR <- object$coef.OR
-  return(list(coef.NT = coef.NT, coef.T = coef.T, coef.OR = coef.OR))
+   coef.longterm <-objectx$coef.longterm
+  return(list(coef.NT = coef.NT, coef.T = coef.T, coef.OR = coef.OR, coef.longterm = coef.longterm))
  }
